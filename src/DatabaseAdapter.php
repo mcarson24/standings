6f4b995b381e5df056f7b012abab31374404ece6
Matthew Carson <matthew24carson@gmail.com>
2019-02-20 12:58:07 -0500

    Refactor/Add doc blocks

--- src/DatabaseAdapter.php
+++ src/DatabaseAdapter.php
@@ -13,25 +13,17 @@ class DatabaseAdapter
 		$this->connection = $connection;
 	}
 
-	public function store($table, $teams)
-	{
-		$this->connection->query(
-			'INSERT INTO teams(team_id, rank, won, lost, first_name, last_name, games_back, last_ten, conference, division, win_percentage, games_played)
-			VALUES(:team_id, :rank, :won, :lost, :first_name, :last_name, :games_back, :last_ten, :conference, :division, :win_percentage, :games_played)',
-			$teams
-		);
-	}
-
-	public function fetchAll($table)
-	{
-		return $this->connection->query("SELECT * FROM {$table}")->fetchAll();
-	}
-
+	/**
+	 * Fetch the data from the teams table.
+	 * 
+	 * @param  string $division 
+	 * @return array           
+	 */
 	public function fetchLeagueStandings($division = '')
 	{
 		$sql = "SELECT rank AS Pos, ";
 
-		if ($division == 'MLB') $sql .= "conference || division AS Div, ";
+		if (! $this->wantsDivisionData($division)) $sql .= "conference || division AS Div, ";
 
 		$sql .= "first_name || ' ' || last_name AS Team,
 				games_played AS GP,
@@ -40,21 +32,51 @@ class DatabaseAdapter
 				games_back as GB,
 				printf('%.3f', win_percentage) AS 'W%',
 				last_ten AS 'L-10'
-		 		FROM teams";
+		 		FROM teams ";
 
-		if ($division != 'MLB') {
-			$sql .= " WHERE conference || division == '{$division}'";
+		if ($this->wantsLeagueData($division)) {
+			$sql .= "WHERE conference || division LIKE '{$division}%' ";
 		}
-		if ($division != 'MLB') {
-			$sql .= " ORDER BY games_back";
+		if ($this->wantsDivisionData($division)) {
+			$sql .= "WHERE conference || division == '{$division}' 
+					 ORDER BY games_back ";
 		}
-		if ($division == 'MLB') {
-			$sql .= " ORDER BY win_percentage DESC";
+		if (! $this->wantsDivisionData($division)) {
+			$sql .= "ORDER BY win_percentage DESC";
 		}
 
 		return $this->connection->query($sql)->fetchAll();
 	}
 
+	/**
+	 * Store new data in the teams table. And set updated_at attribute.
+	 * 
+	 * @param  array $teams 
+	 * @return void        
+	 */
+	public function storeData($teams)
+	{
+		array_map(function($team) {
+			$this->query(
+				"INSERT INTO teams(team_id, rank, won, lost, first_name, last_name, games_back, last_ten, conference, division, win_percentage, games_played)
+				VALUES(:team_id, :rank, :won, :lost, :first_name, :last_name, :games_back, :last_ten, :conference, :division, :win_percentage, :games_played)",
+				$team
+			);
+		}, $teams);
+
+		$this->query(
+			"INSERT INTO updates(updated_at)
+			VALUES(:updated_at);",
+			[Carbon::now()]
+		);
+	}
+
+	/**
+	 * Determine the last time the teams table
+	 * was updated.
+	 * 
+	 * @return string
+	 */
 	public function lastUpdate()
 	{
 		return $this->connection->query("SELECT updated_at
@@ -64,13 +86,49 @@ class DatabaseAdapter
 		")->fetchColumn();
 	}
 
+	/**
+	 * Clear the standings data from the table, so new
+	 * data can replace it.
+	 * 
+	 * @return void
+	 */
 	public function clearStandings()
 	{
 		$this->query("DELETE FROM teams WHERE team_id > 'aaa'");
 	}
 
+	/**
+	 * Run a SQL query without returning 
+	 * any data.
+	 * 
+	 * @param  string $sql        
+	 * @param  array  $parameters 
+	 * @return void             
+	 */
 	public function query($sql, $parameters = [])
 	{
 		$this->connection->prepare($sql)->execute($parameters);
+	}
+
+	/**
+	 * Determines if the user wants data for a league.
+	 * 
+	 * @param  string $division 
+	 * @return boolean           
+	 */
+	private function wantsLeagueData($division)
+	{
+		return in_array($division, ['AL', 'NL']);
+	}
+
+	/**
+	 * Determines if the user wants data for a division.
+	 * 
+	 * @param  string $division 
+	 * @return boolean           
+	 */
+	private function wantsDivisionData($division)
+	{
+		return in_array($division, ['ALE', 'ALC', 'ALW', 'NLE', 'NLC', 'NLW']);
 	}
 }
--- src/ShowCommand.php
+++ src/ShowCommand.php
@@ -12,8 +12,15 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ShowCommand extends Command
 {
+	/**
+	 * The accepted divisions.
+	 * 
+	 * @var array
+	 */
 	protected $divisions = [
 		'MLB' => 'Major League Baseball',
+		'AL'  => 'American League',
+		'NL'  => 'National League',
 		'ALE' => 'American League East',
 		'ALC' => 'American League Central',
 		'ALW' => 'American League West',
@@ -22,6 +29,11 @@ class ShowCommand extends Command
 		'NLW' => 'National League West'
 	];
 
+	/**
+	 * The table headers.
+	 * 
+	 * @var array
+	 */
 	protected $tableHeaders = [
 		'mlb' => ['Pos', 'Div', 'Team', 'GP', 'W', 'L', 'GB', 'W%', 'L-10'],
 		'div' => ['Pos', 'Team', 'GP', 'W', 'L', 'GB', 'W%', 'L-10']
@@ -36,7 +48,7 @@ class ShowCommand extends Command
 	{
 		$this->setName('show')
 			 ->setDescription('Show the current MLB standings')
-			 ->addOption('div', 'd', InputOption::VALUE_OPTIONAL, "Get standings for a single division ['ALE', 'ALC', 'ALW', 'NLE', 'NLC', 'NLW']", 'MLB');
+			 ->addOption('div', 'd', InputOption::VALUE_OPTIONAL, "Get standings for a single division or league ['AL', 'NL', ALE', 'ALC', 'ALW', 'NLE', 'NLC', 'NLW']", 'MLB');
 	}
 
 	/**
@@ -48,29 +60,27 @@ class ShowCommand extends Command
 	 */
 	public function execute(InputInterface $input, OutputInterface $output)
 	{	
-		if (! $this->updatedWithinTheLastDay()) {
-			$this->clearStandings();
-
-			// Load standings from api
-			$teams = $this->fetchStandings();
-			
-			// trim out unwanted information from array
-			$teams = $this->truncate($teams);
-
-			$this->storeData($teams);
-		}
-		
 		$inputOutput = new SymfonyStyle($input, $output);
+		$division = $input->getOption('div');
 
-		if (!$teams = $this->fetchLeagueStandings($input->getOption('div'))) {
-			$inputOutput->warning('That division does not exist.');
-			exit(0);
+		if (! $this->updatedWithinTheLastDay()) {
+			$this->database->clearStandings();
+
+			$this->database->storeData($this->fetchStandings());
 		}
 
-		$inputOutput->title($this->divisions[$input->getOption('div')]);
+		$teams = $this->fetchLeagueStandings($division);
+
+		if (! $this->isAValidDivision($division)) {
+			$inputOutput->warning("That division does not exist. Fetching MLB standings...");
+			$division = 'MLB';
+		}
+
+		$inputOutput->title($this->divisions[$division]);
 		$inputOutput->table(
-			$input->getOption('div') == 'MLB' ? $this->tableHeaders['mlb'] : $this->tableHeaders['div'],
-			$teams);
+			in_array($division, ['MLB', 'AL', 'NL']) ? $this->tableHeaders['mlb'] : $this->tableHeaders['div'],
+			$teams
+		);
 	}
 
 	/**
@@ -84,7 +94,7 @@ class ShowCommand extends Command
 
 		$response = json_decode($client->request('GET', 'https://erikberg.com/mlb/standings.json')->getBody(), true);
 		
-		return $response['standing'];
+		return $this->truncate($response['standing']);
 	}
 
 	/**
@@ -115,42 +125,15 @@ class ShowCommand extends Command
 		}, $teams);
 	}
 
-	private function storeData($teams)
-	{
-		array_map(function($team) {
-			$this->database->query(
-				"INSERT INTO teams(team_id, rank, won, lost, first_name, last_name, games_back, last_ten, conference, division, win_percentage, games_played)
-				VALUES(:team_id, :rank, :won, :lost, :first_name, :last_name, :games_back, :last_ten, :conference, :division, :win_percentage, :games_played)",
-				$team
-			);
-		}, $teams);
-
-		$this->database->query(
-			"INSERT INTO updates(updated_at)
-			VALUES(:updated_at);",
-			[Carbon::now()]
-		);
-	}
-
 	/**
-	 * Determines if the standings have been updated within
-	 * the last day.
+	 * Determines if the database has been updated 
+	 * within the last day.
 	 * 
 	 * @return boolean
 	 */
 	private function updatedWithinTheLastDay()
 	{
 		return Carbon::now()->diffInDays(Carbon::parse($this->database->lastUpdate())) < 1;
-	}
-
-	/**
-	 * Clears the standings data from the teams table.
-	 * 
-	 * @return void
-	 */
-	private function clearStandings()
-	{
-		$this->database->clearStandings();
 	}
 
 	/**
@@ -162,5 +145,16 @@ class ShowCommand extends Command
 	private function fetchLeagueStandings($division = '')
 	{
 		return $this->database->fetchLeagueStandings($division);
+	}
+
+	/**
+	 * Determine if the desired division is valid.
+	 * 
+	 * @param  string  $division 
+	 * @return boolean           
+	 */
+	private function isAValidDivision($division)
+	{
+		return array_key_exists($division, $this->divisions);
 	}
 }
